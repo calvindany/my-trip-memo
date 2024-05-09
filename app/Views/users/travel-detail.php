efd77884e93d48664eb936ab635a51c28c8f5519
inirafli <raplirayhann@gmail.com>
2024-05-09 13:04:59 +0700

    added: detail page functionality with data from database

--- app/Views/users/travel-detail.php
+++ app/Views/users/travel-detail.php
@@ -2,14 +2,14 @@
 
 <?= $this->section('content') ?>
 <!-- Detail Container -->
-<div class="bg-lightgrey min-h-screen my-8 mx-5 flex justify-center font-poppins">
+<div class="bg-lightgrey min-h-screen mt-[86px] mb-8 mx-5 flex justify-center font-poppins">
     <div class="max-w-3xl w-full">
         <!-- Back Button -->
         <div class="pt-2 pb-6">
-            <button class="flex items-center text-redblood">
+            <a href="<?= site_url('/') ?>" class="flex items-center text-redblood">
                 <i class="fas fa-arrow-left mr-3"></i>
                 <p class="font-medium">Back to Home</p>
-            </button>
+            </a>
         </div>
         <!-- Back Button -->
         <div class="bg-white rounded-lg shadow-md overflow-hidden">
@@ -19,34 +19,33 @@
             <div class="p-6">
                 <div class="mb-4">
                     <!-- Memo title -->
-                    <h2 class="text-xl font-semibold mb-2">Trip to Destination</h2>
+                    <h2 class="text-xl font-semibold mb-2"><?= $travelDetail['title'] ?></h2>
                     <!-- Memo title -->
                     <!-- Date & User Row -->
                     <div class="flex flex-col md:flex-row items-start md:items-center mb-2">
                         <div class="flex items-center mr-8 mb-2 md:mb-0">
                             <i class="far fa-calendar text-redblood text-lg mr-3"></i>
-                            <p class="text-redblood font-medium">July 1, 2024</p>
+                            <p class="text-redblood font-medium"><?= date('F j, Y', strtotime($travelDetail['created_at'])) ?></p>
                         </div>
                         <div class="flex items-center">
                             <i class="far fa-user text-redblood text-lg mr-3"></i>
-                            <p class="text-redblood font-medium">Calvin & Rapli</p>
+                            <p class="text-redblood font-medium"><?= $travelDetail['username'] ?></p>
                         </div>
                     </div>
                     <!-- Date & User Row -->
                 </div>
                 <!-- Memo Image & Location -->
                 <div class="mb-8 relative">
-                    <img src="https://via.placeholder.com/800x400" alt="Destination Image" class="w-full rounded-lg">
-                    <div class="absolute bottom-0 right-0 bg-darkgrey text-white px-2 py-1 bg-opacity-25 rounded-br-lg">
-                        <p class="text-xs md:text-sm lg:text-base">Taken at: Universitas Gunadarma</p>
+                    <img src="<?= base_url($travelDetail['thumbnail']) ?>" alt="Destination Image" class="w-full rounded-lg">
+                    <div class="absolute bottom-0 right-0 bg-darkgrey text-white px-2 py-1 bg-opacity-50 rounded-br-lg">
+                        <p class="text-xs md:text-sm lg:text-base">Taken at: <?= $travelDetail['address'] ?></p>
                     </div>
                 </div>
                 <!-- Memo Image & Location -->
                 <!-- Memo Description -->
                 <div>
                     <h3 class="text-lg font-semibold mb-2">Description</h3>
-                    <p class="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus consectetur
-                        libero ut semper.</p>
+                    <p class="text-gray-700"><?= $travelDetail['description'] ?></p>
                 </div>
                 <!-- Memo Description -->
             </div>
