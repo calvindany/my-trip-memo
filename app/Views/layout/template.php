36301de964485c64b159ed6d498ab5864994a82a
calvindany <119773802+calvindany@users.noreply.github.com>
2024-05-09 16:42:33 +0700

    added: improve scroll behavior to smooth

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -17,6 +17,10 @@
      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
     <style>
+      html {
+        scroll-behavior: smooth;
+      }
+
       /* width */
       ::-webkit-scrollbar {
         width: 5px;
